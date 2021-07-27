ad43a1156eb24c6095a5ac832402721251e18e62
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-27 16:50:26 +0300

    creates a login user method

--- solutions/views.py
+++ solutions/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from .models import CreateUserForm
+from .forms import CreateUserForm
+from django.contrib.auth import authenticate, login, logout
+
+
 
 # Create your views here.
 def home(request):
@@ -25,3 +28,21 @@ def register(request):
 
 		context = {'form':form}
 		return render(request, 'registration/register.html', context)
+def loginUser(request):
+	if request.user.is_authenticated:
+		return redirect('index')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('index')
+			else:
+				messages.info(request, 'Username or password is incorrect')
+
+		context = {}
+		return render(request, 'registration/login.html', context)
