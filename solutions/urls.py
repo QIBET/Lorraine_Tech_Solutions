de5fd78d1b9190b57a49aea89fe9452b40041390
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-27 16:00:44 +0300

    creates register function for user

--- solutions/urls.py
+++ solutions/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns=[
     path('',views.home,name = 'home'),
+    path('register/', views.register, name="register"),
+	path('login/', views.loginUser, name="login"), 
+    path('logout/',views.logoutUser,name="logout") ,
 ]
--- solutions/views.py
+++ solutions/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .models import CreateUserForm
 
 # Create your views here.
 def home(request):
     title = "Lorraine Tech Solutions"
     return render(request, 'index.html',{"title":title})
+
+def register(request):
+	if request.user.is_authenticated:
+		return redirect('index')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account was created for ' + user)
+
+				return redirect('login')
+			
+
+		context = {'form':form}
+		return render(request, 'registration/register.html', context)
