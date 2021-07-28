6c01e21abe19e49a918575186f87c4d7dd362504
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-28 10:29:45 +0300

    creates forms for phone and laptop

--- solutions/forms.py
+++ solutions/forms.py
@@ -1,3 +1,4 @@
+from .models import Phone,Laptop
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
@@ -7,3 +8,16 @@ class CreateUserForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
+
+class PhoneForm(forms.ModelForm):
+	
+    class Meta:
+        model = Phone
+        exclude = ['user','date_posted']
+
+class LaptopForm(forms.ModelForm):
+	
+    class Meta:
+        model = Laptop
+        exclude = ['user','date_posted']
+
